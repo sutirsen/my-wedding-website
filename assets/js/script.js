1625d13f7e88dfb456a9fd5be4ccc0d6ca83c685
Sutirtho Sen <sutirtho@amazon.com>
2020-01-29 03:54:45 +0530

    First Draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     new WOW().init();
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Jan 1, 2019 00:00:00").getTime();
+    var countDownDate = new Date("Jan 30, 2020 19:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
